61194edc2308cec60f2869071ab5b7aa7e556789
Andy Gray <andy@whatsthescore.co.uk>
2018-09-11 15:25:41 +0100

    mock safe deliver test

---
+++ contracts/mocks/MockWhitelistedMintableToken.sol
@@ -0,0 +1,11 @@
+pragma solidity 0.4.24;
+
+import "../inx/WhitelistedMintableToken.sol";
+
+contract MockWhitelistedMintableToken is WhitelistedMintableToken {
+
+    // used for false testing
+    function mint(address _to, uint256 _amount) public returns (bool) {
+        return false;
+    }
+}
---
+++ test/inx/MintedKYCCrowdsale.mock.test.js
@@ -0,0 +1,50 @@
+const assertRevert = require('../helpers/assertRevert');
+
+const advanceBlock = require('../helpers/advanceToBlock');
+const increaseTimeTo = require('../helpers/increaseTime').increaseTimeTo;
+const duration = require('../helpers/increaseTime').duration;
+
+const BigNumber = web3.BigNumber;
+
+const should = require('chai')
+  .use(require('chai-as-promised'))
+  .use(require('chai-bignumber')(BigNumber))
+  .should();
+
+const INXCrowdsale = artifacts.require('INXCrowdsale');
+const MockWhitelistedMintableToken = artifacts.require('MockWhitelistedMintableToken');
+
+contract('INXCrowdsale with MockWhitelistedMintableToken', function ([owner, investor, wallet, purchaser]) {
+
+  before(async function () {
+    // Advance to the next block to correctly read time in the solidity "now" function interpreted by testrpc
+    await advanceBlock();
+  });
+
+  beforeEach(async function () {
+    this.token = await MockWhitelistedMintableToken.new({from: owner});
+
+    // setup INX contract!!
+    this.crowdsale = await INXCrowdsale.new(wallet, this.token.address, 1, 2, {from: owner});
+
+    this.openingTime = (await this.crowdsale.openingTime()).toNumber(10);
+    this.minContribution = await this.crowdsale.minContribution(); // 0.2 ETH
+
+    // approve so they can invest in crowdsale
+    await this.crowdsale.addToKyc(purchaser);
+
+    // ensure the crowdsale can transfer tokens - whitelist in token
+    await this.token.addAddressToWhitelist(this.crowdsale.address);
+  });
+
+  describe('BuyTokens', function () {
+
+    beforeEach(async function () {
+      await increaseTimeTo(this.openingTime + duration.seconds(5)); // force time to move on to just after opening
+    });
+
+    it('should revert as token will never deliver (and returns false)', async function () {
+      await assertRevert(this.crowdsale.buyTokens(purchaser, {value: this.minContribution, from: purchaser}));
+    });
+  });
+});
