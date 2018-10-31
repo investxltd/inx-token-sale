3eb0ff40a904441e0d36984ff019e3241f315c5a
Andy Gray <andy@whatsthescore.co.uk>
2018-10-31 10:47:42 +0000

    generic pause tests

--- test/inx/INXCommitment.test.js
+++ test/inx/INXCommitment.test.js
@@ -81,4 +81,98 @@ contract.only('INXCommitment', function ([owner, investor, wallet, unauthorized]
             refunding.should.be.equal(false);
         });
     });
+
+    describe('pause', function () {
+
+        describe('when the contract is unpaused', function () {
+            it('pauses the token', async function () {
+                await this.commitment.pause({from: owner});
+
+                const paused = await this.commitment.paused();
+                paused.should.be.equal(true);
+            });
+
+            it('emits a paused event', async function () {
+                const {logs} = await this.commitment.pause({from: owner});
+
+                assert.equal(logs.length, 1);
+                assert.equal(logs[0].event, 'Pause');
+            });
+        });
+
+        describe('when the token is paused', function () {
+            beforeEach(async function () {
+                await this.commitment.pause({from: owner});
+            });
+
+            it('reverts', async function () {
+                await assertRevert(this.commitment.pause({from: owner}));
+            });
+        });
+
+        describe('when the sender is not the token owner', function () {
+            it('reverts', async function () {
+                await assertRevert(this.commitment.pause({from: unauthorized}));
+            });
+        });
+    });
+
+    // describe('unpause', function () {
+    //     describe('when the sender is the token owner', function () {
+    //         const from = owner;
+    //
+    //         describe('when the token is paused', function () {
+    //             beforeEach(async function () {
+    //                 await this.crowdsale.pause({from});
+    //             });
+    //
+    //             it('unpauses the token', async function () {
+    //                 await this.crowdsale.unpause({from});
+    //
+    //                 const paused = await this.crowdsale.paused();
+    //                 assert.equal(paused, false);
+    //             });
+    //
+    //             it('emits an unpaused event', async function () {
+    //                 const {logs} = await this.crowdsale.unpause({from});
+    //
+    //                 assert.equal(logs.length, 1);
+    //                 assert.equal(logs[0].event, 'Unpause');
+    //             });
+    //         });
+    //
+    //         describe('when the token is unpaused', function () {
+    //             it('reverts', async function () {
+    //                 await assertRevert(this.crowdsale.unpause({from}));
+    //             });
+    //         });
+    //     });
+    //
+    //     describe('when the sender is not the token owner', function () {
+    //         const from = investor;
+    //
+    //         it('reverts', async function () {
+    //             await assertRevert(this.crowdsale.unpause({from}));
+    //         });
+    //     });
+    // });
+
+    describe('commit', function () {
+
+        it('should revert if not owner', async function () {
+            await assertRevert(this.commitment.toggleRefunding({from: unauthorized}));
+        });
+
+        it('should fulfill if owner', async function () {
+            await this.commitment.toggleRefunding({from: owner});
+
+            let refunding = await this.commitment.isRefunding();
+            refunding.should.be.equal(true);
+
+            await this.commitment.toggleRefunding({from: owner});
+
+            refunding = await this.commitment.isRefunding();
+            refunding.should.be.equal(false);
+        });
+    });
 });
